19303b40f9308ee70fbcb79c0843af4d815321fd
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2025-06-17 18:22:39 +0200

    add masonry grid and add note button

--- lib/screens/all_notes_screen.dart
+++ lib/screens/all_notes_screen.dart
@@ -1,9 +1,15 @@
+import 'dart:convert';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook/common/scaffold_content.dart';
 import 'package:flutter_notebook/lib/colors.dart';
 import 'package:flutter_notebook/lib/note_notifier.dart';
 import 'package:flutter_notebook/screens/create_note_screen.dart';
+import 'package:flutter_quill/flutter_quill.dart';
+import 'package:flutter_quill/quill_delta.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class AllNotesScreen extends ConsumerWidget {
   const AllNotesScreen({super.key});
@@ -19,16 +25,11 @@ class AllNotesScreen extends ConsumerWidget {
         title: Text(
           "All Notes",
           style: TextStyle(
-              color: NColors.boldText,
-              fontWeight: FontWeight.w900
+            color: NColors.boldText,
+            fontWeight: FontWeight.w900,
           ),
         ),
-        actions: [
-          IconButton(
-              onPressed: () => {},
-              icon: Icon(Icons.search)
-          )
-        ],
+        actions: [IconButton(onPressed: () => {}, icon: Icon(Icons.search))],
       ),
       body: notesAsync.when(
         data: (notes) {
@@ -41,23 +42,85 @@ class AllNotesScreen extends ConsumerWidget {
                 ),
                 imagePath: "assets/tasks_placeholder.png",
                 title: "Create Your First Note",
-                description: "Add a note about anything (your thoughts on climate change, or your history essay) and share it with the world.",
+                description:
+                "Add a note about anything (your thoughts on climate change, or your history essay) and share it with the world.",
                 buttonText: "Create A Note",
               ),
             );
           }
-          return ListView.builder(
-            itemCount: notes.length,
-            itemBuilder: (context, index) {
-              final note = notes[index];
-              return ListTile(
-                title: Text('Note ${note.id}'),
-                onTap: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => CreateNoteScreen(noteId: note.id)),
+          return Stack(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(bottom: 70),
+                child: MasonryGridView.count(
+                  padding: const EdgeInsets.all(12),
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 12,
+                  crossAxisSpacing: 12,
+                  itemCount: notes.length,
+                  itemBuilder: (context, index) {
+                    final note = notes[index];
+                    final delta = Delta.fromJson(jsonDecode(note.content));
+                    final document = Document.fromDelta(delta);
+                    final plainText = document.toPlainText();
+                    return Card(
+                      elevation: 4,
+                      color: NColors.bgColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        side: BorderSide(color: Colors.grey.shade100),
+                      ),
+                      child: InkWell(
+                        onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                CreateNoteScreen(noteId: note.id),
+                          ),
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.all(8),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Note ${note.id}',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              SizedBox(height: 4),
+                              Text(
+                                plainText,
+                                maxLines: 9,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
                 ),
-              );
-            },
+              ),
+              Positioned(
+                bottom: 40,
+                left: 16,
+                right: 16,
+                child: ElevatedButton(
+                  onPressed: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => CreateNoteScreen()),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(double.infinity, 50),
+                  ),
+                  child: Text(
+                    'Add A Note',
+                    style: TextStyle(color: NColors.boldText),
+                  ),
+                ),
+              ),
+            ],
           );
         },
         loading: () => Center(child: CircularProgressIndicator()),
